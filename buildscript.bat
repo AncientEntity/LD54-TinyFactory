8e87b2aa214546fae7468924d2428b50ce3c1d75
Ryan <ryanhasman10@hotmail.com>
2023-10-01 11:21:22 -0700

    added secret simon music and added sound to buildscript

--- buildscript.bat
+++ buildscript.bat
@@ -1,5 +1,6 @@
 pyinstaller.exe --noconsole main.py --specpath build/spec --distpath build/dist --workpath build/build
 robocopy art build/dist/main/art /E
+robocopy sound build/dist/main/sound /E
 echo COMPLETED
 
 pause
